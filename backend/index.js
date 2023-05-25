9978f566e217dd604bbc307b0b481093bbc11a12
VICTORIA MATTHEWS <olayidecodes@gmail.com>
2023-05-25 09:19:16 +0100

    first commit

---
+++ backend/index.js
@@ -0,0 +1,47 @@
+import express from "express"
+import mysql from "mysql"
+import cors from "cors"
+
+const app = express()
+
+const db = mysql.createConnection({
+    host: "localhost",
+    user: "root",
+    password: "#OLA12codes@",
+    database: "test"
+})
+
+app.use(express.json())
+app.use(cors())
+
+app.get("/", (req, res) => {
+    res.json("Hello")
+})
+
+app.get("/books", (req, res)=> {
+    const q = "SELECT * FROM books"
+    db.query(q, (err, data)=> {
+        if(err) return res.json(err)
+        return res.json(data)
+    })
+})
+
+app.post("/books", (req,res)=> {
+    const q = "INSERT INTO books (`title`, `desc`, `price`, `cover`) VALUES (?)";
+    const values = [
+        req.body.title,
+        req.body.desc,
+        req.body.price,
+        req.body.cover
+    ];
+    db.query(q, [values], (err, data) => {
+        if(err) return res.json(err)
+        return res.json("Book has been created")
+    })
+})
+
+app.listen(8800, ()=> {
+    console.log("Connected to backend!")
+})
+
+// root@localhost
